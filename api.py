9478607bc6362800ae081788324b4c644c22dc68
bavo.denys <bavodenys@gmail.com>
2023-02-28 22:11:07 +0100

    Need to think about scenario where Pmin is bigger than remaining load

--- api.py
+++ api.py
@@ -6,10 +6,40 @@ app = Flask(__name__)
 @app.route('/productionplan', methods=['POST'])
 def determine_production_plan():
     data = request.json
-    response = []
     for powerplant in data['powerplants']:
-        response.append({'name': powerplant['name'],
-                         'p': 0})
+        if powerplant['type'] == 'gasfired':
+            powerplant['cost'] = data['fuels']['gas(euro/MWh)']/powerplant['efficiency']
+        elif powerplant['type'] == 'turbojet':
+            powerplant['cost'] = data['fuels']['kerosine(euro/MWh)']/powerplant['efficiency']
+        elif powerplant['type'] == 'windturbine':
+            powerplant['pmax_op'] = powerplant['pmax']*data['fuels']['wind(%)']/100
+            powerplant['cost'] = 0
+        else:
+            print('other type')
+    remaining_load = data['load']
+    response = []
+    for powerplant in sorted(data['powerplants'], key=lambda d: d['cost']):
+        print(f"{powerplant['name']}:{remaining_load}, {powerplant['pmin']}")
+        if remaining_load == 0:
+            response.append({'name': powerplant['name'],
+                             'p': 0})
+        else:
+            if remaining_load >= powerplant['pmin']:
+                if powerplant['type'] == 'windturbine':
+                    pmax_op = powerplant['pmax_op']
+                else:
+                    pmax_op = powerplant['pmax']
+                if pmax_op < remaining_load:
+                    response.append({'name': powerplant['name'],
+                                     'p': pmax_op})
+                    remaining_load = remaining_load - pmax_op
+                else:
+                    response.append({'name': powerplant['name'],
+                                     'p': remaining_load})
+                    remaining_load = 0
+            else:
+                pass
+
     return jsonify(response)
 
 @app.route('/')
