428fe1315e0a88b8ffddedaff5e80bf6bbac6c9d
bavo.denys <bavodenys@gmail.com>
2023-03-01 21:11:02 +0100

    Save responses on payloads in folder

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 
 API_URL = "http://127.0.0.1:8888/"
 PAYLOAD_FOLDER = "example_payloads"
+RESPONSE_FOLDER = "example_response"
 
 if __name__ == "__main__":
     # Test the API connection
@@ -15,7 +16,10 @@ if __name__ == "__main__":
             with open(f'{PAYLOAD_FOLDER}/{payload}', 'r') as f:
                 data = json.load(f)
                 result = requests.post(API_URL + "/productionplan", json=data).json()
-
+                f.close()
+            with open(f"{RESPONSE_FOLDER}/response_{payload}", "w") as f:
+                json.dump(result, f)
+                f.close()
     else:
         print('Test of API connection was not successful')
 
